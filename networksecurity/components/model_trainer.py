2e5c15a9744585213ee3055a4b75bb2649d83764
Dhruvpatel00 <dhruvh.patel213@gmail.com>
2025-01-22 17:47:22 +0530

    pipeline is completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -26,6 +26,8 @@ from sklearn.ensemble import (
 import mlflow
 from urllib.parse import urlparse
 
+import dagshub
+dagshub.init(repo_owner='dhruvh.patel213', repo_name='NetworkSecurity_DataScience', mlflow=True)
 
 
 
